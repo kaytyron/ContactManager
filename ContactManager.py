a6b415308e2cf1d70c7f60759a5cd0f89460ca12
kaytyron <kelvin.tyron@gmail.com>
2017-09-19 18:09:49 +0000

    Working Basic Manager

--- ContactManager.py
+++ ContactManager.py
@@ -1,11 +1,4 @@
-class Contact():
-    def __init__(self, name, phone_number, email, gender, post_address ):
-        self.name = name
-        self.phone_number = phone_number
-        self.gender = gender
-        self.email = email
-        self.post_address = post_address
-        
+class Contact():     
     def set_name(self):
         self.name = str(input("Enter name: \n"))
 
@@ -39,55 +32,59 @@ class Contact():
     def get_contact(self):
         print("CONTACT DETAILS\nName: {0}\nPhone: {1}\nEmail: {2}\nGender: {3}\nPostal address: {4}\n".format(self.get_name(), self.get_phone(), self.get_email(), self.get_gender(), self.get_post()))
 
-if __name__ == "__main__":
 
-    contact_holder = list()
 
-    code = -1
 
-    while( code != 0):
-        code = int(input("Enter 1 to add contact, 2 to delete, 3 to search, 0 to exit :\n"))
+class ContactManager:
+    def __init__(self):
+        self.contact_holder = list()
+        
 
-        if code == 0:
-            #exit loop
-            break
+    def add_contact(self):
+        print("Adding a New Contact:")
+        tempContact = Contact()
 
-        elif code == 1:
-            #add contact
-            print("Adding a New Contact:")
-            newContact.set_name()
-            newContact.set_post()
-            newContact.set_gender()
-            newContact.set_email()
-            newContact.set_phone()
-            contact_holder.append(newContact)
-            print("Contact added\n")
+        tempContact.set_name()
+        tempContact.set_post()
+        tempContact.set_gender()
+        tempContact.set_email()
+        tempContact.set_phone()
+        self.contact_holder.append(tempContact)
+        print("Contact added\n")
 
-        elif code == 2:
-            #delete contact
-            parameter = input("Enter name to delete:")
-            index = 0
-            for present_contact in contact_holder:
-                if present_contact.name == parameter:
-                    contact_holder.remove(present_contact)
-                    index += 1
-                    print("Contact Deleted!\n")
-                else:
-                    print("Contact not Found.\n")
-            
-        elif code == 3:
-            #search contact
-            parameter = input("Enter name to search:")
-            index = 0
-            for present_contact in contact_holder:
-                if present_contact.name == parameter:
-                    present_contact.get_contact()
-                    index += 1
-                else:
-                    print("Name not found\n")
+    def delete_contact(self):
+        parameter = input("Enter name to delete:")
+        index = 0
+        for present_contact in self.contact_holder:
+            if present_contact.name == parameter:
+                self.contact_holder.remove(present_contact)
+                index += 1
+                print("Contact Deleted!\n")
+            else:
+                print("Contact not Found.\n")
 
+    def search_contact(self):
+        search_name = input("Enter name to search:")
+        index = 0
+        found_contact = list()
+        for present_contact in self.contact_holder:
+            if present_contact.name == search_name:
+                found_contact.append(present_contact)
+            index += 1
+        
+        if len(found_contact) == 0:
+            print("Contact not found")
         else:
-            print("Not a valid option. Kindly Re-enter a number.")        
+            for one in found_contact:
+                one.get_contact()
 
+
+            
+
+
+newContact = ContactManager()
+newContact.add_contact()
+newContact.delete_contact()
+newContact.search_contact()
 
             
